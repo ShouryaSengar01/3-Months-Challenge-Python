a3d5b9d1d2fd5cc90366a041011e533690a786ba
ShouryaSengar01 <shouryasengar07@gmail.com>
2025-05-03 18:44:52 +0530

    Update and rename Solution.py to Day 1.py

---
+++ week-1/Day 1.py
@@ -0,0 +1,36 @@
+# I completed all the lessons till Python casting in w3 schools along with exercises.
+# Watched the video mentioned in my 3 month bootcamp till 40 minutes time stamp completing all the concepts till working with strings. Will watch more of it tomorrow and complete at least the calculator project.
+# Below are the Notes I made. 
+
+# VARIABLE NOTES 
+#Variable = A variable is a container for values like strings, integer, float and booleans. Variables are Case Sensitive.
+
+#STRING
+Name = "Sharry"
+Food = "Subway"
+Age = "19"
+
+#I'm using f-string here
+print(f"My Name is {Name}")
+print(f"My favourite food is {Food} and Chocolate cookies") #Basically Anything B/w these "" is a String.
+print(type(Name))
+#INTEGER
+
+age = 19
+currency = 25
+
+print(f"My age is {age}")
+print(f"You are paying {currency:.2f} bucks") # :.nf is used to add decimals after an integer, where n = amount of decimals u want.
+print(type(age))
+
+#FLOAT
+Amount = 22.87
+print(f"Your total will be{Amount}")
+print(type(Amount))
+
+#BOOLEAN
+A = True #first letter should be capital
+B = False #first letter should be capital
+
+# Here is the Link for W3 school exercises I completed:- 
+# https://imagekit.io/tools/asset-public-link/?detail=%7B%22name%22%3A%22screenshot_1746277624277.png%22%2C%22type%22%3A%22image%2Fpng%22%2C%22signedurl_expire%22%3A%222028-05-02T13%3A06%3A56.081Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F7384b997ed104255%2Fscreenshot_1746277624277.png%3FExpires%3D1840885616%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DGld1Tz5sZ6NX9mphU8OXX0R6uATCK6-YAm8KqjA6-KWzckGGNRVwLnOfIOR9wpLGZctdrG5LYY1t1jGPMjVCGf-yVFQSSwjdDWyRNukZMATJMvKSWsU3cbnnU0Zgig0MMNUCeucQauferKHNrQMC13LuQdfbJlHIG5sn-YPG1Xms6C4ujOdSuYgvd12l3X0d860tT1p5edlAKuMF3z4edoQp4d60wwVIIa~3R3rN6uCTuxvRJTSwYYZA01378UtR4O4ByCWyphasqVK3aoQ3ZMKdcjZblEAASfWhGexGb-34n3LNSj7uQBPpSwa0xlcd3pz4YNTGo0VxDTp-NNNWZQ__%22%7D
