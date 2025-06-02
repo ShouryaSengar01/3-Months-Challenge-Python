99efd285b4560ed52aeab3e47fa9f821ac3aa2a7
ShouryaSengar01 <shouryasengar07@gmail.com>
2025-06-02 14:37:01 +0530

    Update Day-7.py

--- Week-4/Day-7.py
+++ Week-4/Day-7.py
@@ -9,3 +9,6 @@ for _ in range(timer,0,-1):
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 print("TIME'S UP")
+
+
+#I did this a lot before but forgot to upload so i uploaded this on day 7 coz i didn't do much on this day, went thru w3 school and revision n stuff only
