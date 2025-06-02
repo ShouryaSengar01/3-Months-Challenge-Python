ead2922bac10aaa0cd18829ef6fe400b96a4c839
ShouryaSengar01 <shouryasengar07@gmail.com>
2025-06-02 14:35:16 +0530

    Create Day-7.py

---
+++ Week-4/Day-7.py
@@ -0,0 +1,11 @@
+import time
+
+timer = int(input("Please Enter the time in Seconds: "))
+
+for _ in range(timer,0,-1):
+    seconds = _ % 60
+    minutes = int(_/60) % 60
+    hours = int(_ / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("TIME'S UP")
